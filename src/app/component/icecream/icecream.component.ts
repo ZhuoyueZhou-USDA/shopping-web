4e2d8500517f30489ddaa763f039bd2fd605b2bb
kiki <xiawucha0908@gmail.com>
2018-11-02 17:38:04 -0400

    ice cream page init

--- src/app/component/icecream/icecream.component.ts
+++ src/app/component/icecream/icecream.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IcecreamComponent implements OnInit {
 
+  listShow = false;
   constructor() { }
 
+    showList() {
+    this.listShow = !this.listShow;
+    }
   ngOnInit() {
   }
 
